3a006522d8c6f7f72280320758bc1409b6261ff6
Joan Jefferson Talizo Balbin <72896411+Kaitozerob@users.noreply.github.com>
2024-05-03 17:11:12 -0500

    Add files via upload

---
+++ src/users/profile/components/profile.component.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  name: 'UserProfile',
+  data() {
+    return {
+      user: {
+        name: 'Juan Perez',
+        email: 'juanperez@example.com',
+        profilePicture: 'assets/assistant.png'
+      }
+    };
+  }
+}
+
+
+</script>
+
+<template>
+  <div class="user-profile">
+    <img :src="user.profilePicture" alt="Profile Picture">
+    <h1>{{ user.name }}</h1>
+    <p>Email: {{ user.email }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+.user-profile img {
+  width: 100px;
+  height: auto;
+  border-radius: 50%;
+}
+.user-profile h1 {
+  font-size: 20px;
+}
+</style>
---
+++ src/users/profile/pages/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="profile-view">
+    <user-profile></user-profile>
+  </div>
+</template>
+
+<script>
+import Profile from '../components/profile.component.vue';
+
+export default {
+  components: {
+    Profile
+  }
+}
+</script>
